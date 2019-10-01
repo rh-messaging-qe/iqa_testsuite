3137410e0b1916a64435c2223b36f93935ac6b9e
Nicolas Brignone <nbrignon@redhat.com>
2019-10-01 17:16:36 -0300

    all together for now, commit history will be fixed.

--- integration/int_3Ri_2BhaRi2_3Re_2BhaRe3/test_multicast_basic.py
+++ integration/int_3Ri_2BhaRi2_3Re_2BhaRe3/test_multicast_basic.py
@@ -4,6 +4,7 @@ import logging
 import ast
 import time
 import os
+import random
 
 import pytest
 from messaging_abstract.message import Message
@@ -13,18 +14,76 @@ from pytest_iqa.instance import IQAInstance
 from integration.int_3Ri_2BhaRi2_3Re_2BhaRe3.receiver import Receiver
 from integration.int_3Ri_2BhaRi2_3Re_2BhaRe3.sender import Sender
 
-class Outcome: pass
-Outcome.accept = "accept"
-Outcome.release = "release"
-Outcome.modify = "modify"
-Outcome.reject = "reject"
+class Outcome:
+    accept = "accept"
+    release = "release"
+    modify = "modify"
+    reject = "reject"
+
+class Expected:
+    accepted = "accepted"
+    released = "released"
+    modified = "modified"
+    rejected = "rejected"
+
+def idfn(outcome):
+    return outcome["test_id"]
+
+outcomes_config_list = [
+    {
+     "recv_outcomes": 4*[Outcome.accept],
+     "expected": Expected.accepted,
+     "test_id": "Expect accepted if all accept.",
+    },
+    {
+     ## expect REJECTED if any reject:
+     "recv_outcomes": [Outcome.reject, Outcome.modify, Outcome.release],
+     "expected": Expected.rejected,
+     "test_id": "Expect rejected if any reject.",
+    },
+    {
+     "recv_outcomes": [Outcome.reject, Outcome.release],
+     "expected": Expected.rejected,
+     "test_id": "Expect rejected if any reject.",
+    },
+    #two issues here:
+    #1) proton sender counts modified as released
+    #2) the "Expect accept if no rejects" (taken from system tests) fails but according to documentation...
+    #probably it should fail, from documentation:
+    #accepted means:
+    # - All consumers received the message,
+    # - Or, at least one consumer received the message, but no consumers rejected it.
+    #in this case no consumer is accepting ...
+    #{
+     #"recv_outcomes": [Outcome.modify, Outcome.release],
+     #"expected": Expected.accepted,
+     #"test_id": "Expect accept if no rejects=====",
+    #},
+    #{
+     #"recv_outcomes": 3*[Outcome.release] + [Outcome.modify],
+     #"expected": Expected.modified,
+     #"test_id": "Expect modified over released=====",
+    #},
+    #{
+     #"recv_outcomes": 2*[Outcome.modify],
+     #"expected": Expected.modified,
+     #"test_id": "Expected modify if all modify",
+    #},
+    {
+     "recv_outcomes": 6*[Outcome.release],
+     "expected": Expected.released,
+     "test_id": "Release only if all released",
+    },
+]
+
+@pytest.fixture(params=outcomes_config_list, ids=idfn)
+def outcomes(request):
+    return request.param
 
 class _Receiver(Receiver):
     def __init__(self, *args, settle=Outcome.accept, **kwargs):
-        super(_Receiver, self).__init__(*args, **kwargs)
-        self.auto_accept = kwargs["auto_accept"]
-        if not self.auto_accept:
-            self._settle = getattr(self, settle)
+        super(_Receiver, self).__init__(*args, auto_accept=False, **kwargs)
+        self._settle = getattr(self, settle)
 
     def modify(self, delivery):
         super(_Receiver, self).release(delivery, delivered=True)
@@ -33,132 +92,111 @@ class _Receiver(Receiver):
         super(_Receiver, self).release(delivery, delivered=False)
 
     def on_message(self, event):
-        """
-        Processes an incoming message
-        :param event:
-        :return:
-        """
-
-        # Ignore received message from user id
-        if self.ignore_dups and event.message.user_id and event.message.id and \
-                event.message.user_id in self.last_received_id and \
-                self.last_received_id[event.message.user_id] == event.message.id:
-            logging.warning('Ignoring duplicated message [id: %s]' % event.message.id)
-            return
-
-        logging.debug("%s - received message" % self.container_id)
         self.last_received_id[event.message.user_id] = event.message.id
         self.received += 1
+        self.messages.append(event.message.body)
 
-        # Saving received message for further validation
-        if self.save_messages:
-            self.messages.append(event.message.body)
+        logging.info("settle = %s" % self._settle.__name__)
+        self._settle(event.delivery)
 
-        if not self.auto_accept:
-            logging.error("===== _settle ==== {} ".format(self._settle.__name__))
-            self._settle(event.delivery)
-
-        # Validate if receiver is done receiving
         if self.is_done_receiving():
             self.stop_receiver(event.receiver, event.connection)
 
-class TestMulticast:
-    MESSAGES = 5
-    MESSAGE_SIZE = 128
-    MESSAGE_BODY = (("ABCDEFGHIJKLMNOPQRSTUVWXYZ" * math.ceil(MESSAGE_SIZE / 10))[:MESSAGE_SIZE])
-    MESSAGE_SHA1SUM = hashlib.sha1(MESSAGE_BODY.encode('utf-8')).hexdigest()
+class _Sender(Sender):
+    def is_done_sending(self):
+        return (self.stopped or (self.total > 0 and self.sent == self.total))
 
-    TIMEOUT = 6
+class TestMulticast:
+    MESSAGES_COUNT = 5
+    MESSAGE_SIZE = 128
+
+    TIMEOUT = 4 #why?
     address = "multicast/bla"
 
     @staticmethod
     def _get_router_url(router, topic):
-        """
-        Returns an "amqp" url to connect with the given router / topic
-        :param router:
-        :param topic:
-        :rtype: str
-        :return:
-        """
         return "amqp://%s:%s/%s" % (router.node.get_ip(), router.port, topic)
 
-    def _sender(self, router, topic, auto_settle=True):
-        s = Sender(url=self._get_router_url(router, topic),
-                   message_count=self.MESSAGES,
+    def _sender(self, router, topic):
+        s = _Sender(url=self._get_router_url(router, topic),
+                   message_count=self.MESSAGES_COUNT,
                    sender_id='sender-%s' % router.node.hostname,
                    timeout=self.TIMEOUT,
                    message_size=self.MESSAGE_SIZE,
-                   use_unique_body=True)
+                   use_unique_body=True,
+                   auto_settle=False,
+                  )
 
         s.start()
         return s
 
-    def _receiver(self, router, topic, auto_accept=True, settle=Outcome.modify):
+    def _receiver(self, router, topic, settle=Outcome.modify):
         r = _Receiver(url=self._get_router_url(router, topic),
-                              message_count=self.MESSAGES,
-                              settle=settle,
-                              timeout=self.TIMEOUT,
-                              save_messages=True,
-                              auto_accept=auto_accept)
-
-        # Starting subscriber
+                      message_count=self.MESSAGES_COUNT,
+                      settle=settle,
+                      timeout=self.TIMEOUT,
+                      )
         r.start()
         return r
 
-    def test_base_multicast_rejected(self, iqa: IQAInstance):
-        l = [
-            [Outcome.accept, Outcome.reject],
-            [Outcome.reject, Outcome.reject],
-        ]
-        for outcomes in l:
-            sender = self._test_base_multicast(iqa, outcomes[0], outcomes[1])
-            assert sender.rejected == self.MESSAGES
-
-    def test_base_multicast_accepted(self, iqa: IQAInstance):
-        sender = self._test_base_multicast(iqa, Outcome.accept, Outcome.accept)
-        assert sender.confirmed == self.MESSAGES
-
-    def _test_base_multicast(self, iqa: IQAInstance, outcome_r1, outcome_r2):
-
-        def _wait(*args):
-            for r in args:
+    def launch_receivers(self, outcomes, iqa):
+        def _wait(receivers):
+            for r in receivers:
                 while not r.receiver:
                     time.sleep(1)
 
-        def _join(*args):
-            for p in args:
-                p.join()
+        all_routers = iqa.get_routers()
+        routers = random.sample(all_routers, len(outcomes))
+        receivers = []
+        for idx, router in enumerate(routers):
+            receivers.append(self._receiver(router, self.address,
+                                            settle=outcomes[idx]))
+        _wait(receivers)
+        return receivers
 
-        routers = iqa.get_routers()
-        router_send = iqa.get_routers('Router.E1')[0]
-        router_recv_1 = iqa.get_routers('Router.E2')[0]
-        router_recv_2 = iqa.get_routers('Router.E3')[0]
+    def test_base_multicast(self, iqa: IQAInstance, router, outcomes):
 
-        r1 = self._receiver(router_recv_1, self.address, auto_accept=False, settle=outcome_r1)
-        r2 = self._receiver(router_recv_2, self.address, auto_accept=False, settle=outcome_r2)
+        def _wait_for_all_process_to_terminate(threads):
+            for t in threads:
+                t.join()
 
-        _wait(r1, r2)
+        def _assert_all_receivers_messages(receivers, expected):
+            for r in receivers:
+                assert r.messages == expected
 
-        #auto_settle not working?
-        s = self._sender(router_send, self.address, auto_settle=True)
+        def _assert_sender_expected_settlement(sender, expected):
+            assert sender.settled == self.MESSAGES_COUNT
+            for e in [Expected.accepted, Expected.released,
+                      Expected.rejected, Expected.modified]:
 
-        #assert r.received == expected_count
-        #assert r.messages == losmensajes
-        #assert s.sent == expected_count
+                outcome_count = getattr(sender, e)
+                if e == expected:
+                    assert outcome_count == self.MESSAGES_COUNT
+                else:
+                    assert outcome_count == 0
 
-        _join(r1, r2, s)
+        router_send = router
 
-        logging.error("====== sent_id: {}, sent_sent: {}".format(s.sender_id, s.sent))
-        logging.info("""====== sent: accepted: {}, rejected: {} released: {}""".format(
-                      s.confirmed,
-                      s.rejected,
-                      s.released))
+        receivers = self.launch_receivers(outcomes["recv_outcomes"], iqa)
+        sender = self._sender(router_send, self.address)
 
-        for r in [r1, r2]:
-            logging.error("====== messages: {}".format(r.messages))
+        _wait_for_all_process_to_terminate(receivers + [sender])
 
-        assert r1.messages == r2.messages
-        assert len(r1.messages) == self.MESSAGES
+        logging.info("sender_id: {}".format(sender.sender_id))
+        assert sender.sent == self.MESSAGES_COUNT
 
-        return s
+        logging.info("""sent: accepted: {}
+                        rejected: {}
+                        released: {}
+                        modified: {}
+                        settled {}
+                     """.format(
+                      sender.accepted,
+                      sender.rejected,
+                      sender.released,
+                      sender.modified,
+                      sender.settled))
 
+        _assert_all_receivers_messages(receivers,
+                                       expected = [sender.message_body] * self.MESSAGES_COUNT)
+        _assert_sender_expected_settlement(sender, outcomes["expected"])
